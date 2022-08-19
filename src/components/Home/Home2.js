50986d76196bdf7a4d45224489d061a6f6624fa0
Brian Nyachae <techconsultants.bree@gmail.com>
2022-08-20 00:35:17 +0300

    Update Home2.js
    fix typos

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -32,7 +32,7 @@ function Home2() {
                 <b className="purple">Web Technologies and Products </b> and
                 also in areas related to{" "}
                 <b className="purple">
-                  Deep Learning and Natural Launguage Processing.
+                  Deep Learning and Natural Language Processing.
                 </b>
               </i>
               <br />
